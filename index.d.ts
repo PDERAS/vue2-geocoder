cbfb71389ebb2c8b9c6a470e918f2b12aba04478
simon-lanf <58534677+simon-lanf@users.noreply.github.com>
2020-11-18 15:03:42 -0500

    optional location object fields

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,10 @@
 interface Vue2GeocoderLocationObj {
-    address_line_1: string;
-    address_line_2: string;
-    city: string;
-    state: string;
-    zip_code: string;
-    country: string;
+    address_line_1?: string;
+    address_line_2?: string;
+    city?: string;
+    state?: string;
+    zip_code?: string;
+    country?: string;
 }
 
 interface Vue2GeocoderLatLngObj {
